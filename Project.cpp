c8f1102fd0ba2f1a3fd402066f0694cd358bd526
gaziqureshi <gaziqureshi12@gmail.com>
2025-11-20 20:03:40 +0530

    Create Project.cpp

---
+++ Project.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 7;
+    int sum = a + b;
+
+    cout << "First number: " << a << endl;
+    cout << "Second number: " << b << endl;
+    cout << "Sum: " << sum << endl;
+
+    return 0;
+}
